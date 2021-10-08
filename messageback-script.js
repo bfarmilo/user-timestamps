913b89440d985366295e483d7fff339c0e61d8ce
Bill Farmilo <bill@farmilostrategy.com>
2021-10-08 12:13:03 -0400

    initial working commit

--- messageback-script.js
+++ messageback-script.js
@@ -1,10 +1,19 @@
 // document.querySelector('button').addEventListener('click', function() {
 //  chrome.extension.sendMessage({action: 'message', content:"Changed by page"}, function(message){});
 // });
-document.querySelector('button').addEventListener('click', function() {
-    sendObjectToDevTools({content: "Changed by page"});
+/* document.querySelector('button').addEventListener('click', function () {
+    sendObjectToDevTools({ content: "Changed by page" });
+}); */
+
+document.addEventListener('click', function (e) {
+    sendObjectToDevTools({ action: 'Clicked', what:`${e.target.tagName}: ${e.target.innerText || e.target.id || e.target.className}` });
 });
+
+document.addEventListener('keydown', function (e) {
+    sendObjectToDevTools({ action: 'Typed', keyVal: e.key })
+})
+
 function sendObjectToDevTools(message) {
     // The callback here can be used to execute something on receipt
-    chrome.extension.sendMessage(message, function(message){});
+    chrome.runtime.sendMessage(message, function (message) { console.log(message.content)});
 }
